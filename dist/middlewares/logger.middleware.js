d6127a0c990e5ddda194014a020111d073dcce3c
Dhalintin <uchexdhalitin@gmail.com>
2025-09-09 21:18:08 +0100

    Chore: pino fix

--- dist/middlewares/logger.middleware.js
+++ dist/middlewares/logger.middleware.js
@@ -1,19 +1,33 @@
 "use strict";
+// // src/utils/logger.ts
+// import pino from "pino";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// const logger = pino({
+//   transport: {
+//     target: "pino-pretty",
+//     options: {
+//       colorize: true,
+//       translateTime: "SYS:standard",
+//       ignore: "pid,hostname",
+//     },
+//   },
+//   level: "info",
+// });
+// // export default logger;
 // src/utils/logger.ts
 const pino_1 = __importDefault(require("pino"));
-const logger = (0, pino_1.default)({
+const isDevelopment = process.env.NODE_ENV === "development";
+const logger = (0, pino_1.default)(Object.assign({ level: "info" }, (isDevelopment && {
     transport: {
-        target: 'pino-pretty',
+        target: "pino-pretty",
         options: {
             colorize: true,
-            translateTime: 'SYS:standard',
-            ignore: 'pid,hostname'
-        }
+            translateTime: "SYS:standard",
+            ignore: "pid,hostname",
+        },
     },
-    level: 'info'
-});
+})));
 exports.default = logger;
